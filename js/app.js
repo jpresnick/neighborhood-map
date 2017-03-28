cc8670723c38d89289da56a316a9b4b3bc21d405
jpresnick <jpresnic@eckerd.edu>
2017-03-28 16:05:37 -0400

    Refactor code to fit MVVM structure

--- js/app.js
+++ js/app.js
@@ -1,52 +1,42 @@
-var Model = function() {
-
-
-}
-
-var ViewModel = function() {
-
-}
-
-var View = function() {
-
-}
-
-
-
-
-
-
-
-// Creates the map
-var map;
-var markers = [];
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 27.762315, lng: -82.695607},
-		zoom: 12
-	});
-	var locations = [
+var model = {
+	locations: [
 		{title: 'Rollbotto Sushi', location: {lat: 27.774163, lng: -82.6337226}},
 		{title: "Skyway Jack's Restaurant", location: {lat: 27.7435622, lng: -82.679839}},
 		{title: "Larry's Ice Crean & Gelatos", location: {lat: 27.7364418, lng: -82.74735029999999}},
 		{title: 'Pass-A-Grille Beach', location: {lat: 27.6956855, lng: -82.73673719999999}},
 		{title: "Mazzaro's Italian Market", location: {lat: 27.792417, lng: -82.673615}},
-		{title: "Crawley's Downtown", location: {lat: 27.7712088, lng: -82.6367765}}
-	];
+		{title: "Crawley's Downtown", location: {lat: 27.7712088, lng: -82.6367765}},
+		{title: "Hook's Sushi Bar & Thai Food", location: {lat: 27.7837765, lng: -82.6469384}}
+	]
 
-	for (var i = 0; i < locations.length; i++){
-		var position = locations[i].location;
-		var title = locations[i].title;
-		var marker = new google.maps.Marker({
-			map: map,
-			position: position,
-			title: title,
-			id: i
+}
+
+// ViewModel
+var ViewModel = {
+	
+}
+
+var View = {
+	initMap: function(){
+		var markers = [];
+	    // Constructor creates a new map - only center and zoom are required.
+		var map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 27.762315, lng: -82.695607},
+			zoom: 12
 		});
-		markers.push(marker);
-	}
 
+		for (var i = 0; i < model.locations.length; i++){
+			var position = model.locations[i].location;
+			var title = model.locations[i].title;
+			var marker = new google.maps.Marker({
+				map: map,
+				position: position,
+				title: title,
+				id: i
+			});
+			markers.push(marker);
+		}
+	}
 }
 
 // This block of code contains the functionality for the slide out list
